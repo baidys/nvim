5b1a1591472bb6c2c96c172b3b2deb06325d139e
baidys <baidys@hablly.com>
2025-09-13 21:38:09 +0200

    initial commit

---
+++ lua/options.lua
@@ -0,0 +1,8 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.clipboard = 'unnamedplus'
+
+vim.cmd('set noshowmode')
+vim.opt.laststatus = 0
+vim.opt.ruler = false
+vim.cmd("colorscheme leaf")
